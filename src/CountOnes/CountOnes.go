24a47d9853a9730031b1b9c5038125725ffb59b3
JJ Merelo <jjmerelo@gmail.com>
2017-05-01 18:35:19 +0200

    Making it simpler for shorter strings

--- src/CountOnes/CountOnes.go
+++ src/CountOnes/CountOnes.go
@@ -1,12 +1,39 @@
 package CountOnes
 
+import (
+	"fmt"
+	"strconv"
+)
+
+var unos_8bits = make(map[string]int)
+
+// Initialize 8 bit values
+func init() {
+	var i int64
+	for i = 0; i < 256; i++ {
+		this_string := fmt.Sprintf("%-8s", strconv.FormatInt(i,2) )
+		unos := 0
+		for j:= 0; j < 8; j++ {
+			if ( this_string[j] == '1' ) {
+				unos++
+			}
+		}
+		unos_8bits[this_string] = unos
+	}
+}
+
 // Check out https://gobyexample.com/command-line-arguments
-func count( cadena string) int {
+func count( cadena string ) int {
 	unos := 0
-	for _, element := range cadena {
-		if ( element == '1' ) {
-			unos++
-		} 
+	fmt.Printf( "Cadena longitud %d ", len( cadena ))
+	if len( cadena ) > 8  {
+		for _, element := range cadena {
+			if ( element == '1' ) {
+				unos++
+			}
+		}
+	} else {
+		unos = unos_8bits[ cadena ]
 	}
 	return unos
 }
--- src/CountOnes/CountOnes_test.go
+++ src/CountOnes/CountOnes_test.go
@@ -5,12 +5,21 @@ import (
 )
 
 
-func test16(t *testing.T) {
+func TestShort(t *testing.T) {
+	t.Log("Test 8 bits")
+	if ones_8bits := count("10101010"); ones_8bits != 4 {
+		t.Error("Esperaba 4, pero obtuve ", ones_8bits)
+	}
+}
+
+func TestLonger(t *testing.T) {
 	t.Log("Test 16 bits")
 	if ones := count("1010101010101010"); ones != 8 {
 		t.Error("Esperaba 8, pero obtuve ", ones)
 	}
+}
 
+func TestZero(t *testing.T) {
 	t.Log("Test 0")
 	if cero := count("00000000000000000000000000000000000000000"); cero != 0 {
 		t.Error("Esperaba 8, pero obtuve ", cero)
